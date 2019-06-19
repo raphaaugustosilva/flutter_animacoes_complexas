b20be2cb85e9b70452606bbdb52a7e4588167888
Raphael Silva <raphael.silva@sage.com>
2019-06-19 17:09:24 -0300

    Projeto inicial sem implementação

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'helper/navegacaoHelper.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: "/",
+      onGenerateRoute: NavegacaoHelper.rotas(),
+      onUnknownRoute: NavegacaoHelper.rotaNaoEncontrada(),
+      title: "Animações Complexas",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
---
+++ lib/view/principalView.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class PrincipalView extends StatefulWidget {
+  @override
+  _PrincipalViewState createState() => _PrincipalViewState();
+}
+
+class _PrincipalViewState extends State<PrincipalView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Animações complexas"),
+        centerTitle: true,
+      ),
+      body: Container(),
+    );
+  }
+}
